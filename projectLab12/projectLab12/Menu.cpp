5023d6037988b120b8497d7c97ee5f09dd12cce3
RicoygLex <ricoyserglex@gmail.com>
2023-11-28 15:45:43 +0300

    Project added

---
+++ projectLab12/projectLab12/Menu.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "guessNumber.h"
+#include "snake.h"
+
+using namespace std;
+void main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "ru");
+	char choice;
+	do
+	{
+		system("cls");
+		cout << "Выберите игру" << endl;
+		cout << "1 - Змейка" << endl;
+		cout << "2 - Угадать цифру" << endl;
+		cout << "3 - Выход" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case '1':
+			SnakeMain();
+			break;
+		case '2':
+			GuessNumMain();
+			break;
+		case '3':
+			break;
+		}
+	} while (choice != 3);
+	system("pause");
+}
